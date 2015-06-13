52e6723cdb0dd64bccde1586d2a56b05e0a432fc
Brendan Richards <brendan_richards_msdn@hotmail.com>
2015-06-13 19:23:24 +1000

    Lazy Loading Off

---
+++ SSW.DataOnion.UserGroupDemo/SSW.DataOnion.UserGroupDemo.VanillaEF.IntegrationTests/IncludeTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SSW.DataOnion.UserGroupDemo.Domain;
+using TestStack.BDDfy;
+using Xunit;
+
+namespace SSW.DataOnion.UserGroupDemo.VanillaEF.IntegrationTests
+{
+    public class IncludeTest
+    {
+
+
+        void GivenBrendanHasEnrolments()
+        {
+            DropDatabase.DropDb();
+            using (var context = new DemoContextFactory().Create())
+            {
+                var brendan = context.Students.FirstOrDefault(s => s.FirstName == "Brendan");
+
+                Assert.NotNull(brendan);
+
+                brendan.Enrolments.Add(new Enrolment()
+                {
+                    StartDate = DateTime.Now,
+                    EndDate = DateTime.Now.AddYears(1)
+                });
+
+                brendan.Enrolments.Add(new Enrolment()
+                {
+                    StartDate = DateTime.Now,
+                    EndDate = DateTime.Now.AddYears(1)
+                });
+
+                context.SaveChanges();
+            }
+        }
+
+
+        void WhenIUseIncludeEnrolmentsAreProvided()
+        {
+            using (var context = new DemoContextFactory().Create())
+            {
+                context.Configuration.LazyLoadingEnabled = false;
+
+                var brendan = context.Students
+                    .Include(s => s.Enrolments)
+                    .Include(s => s.Courses)
+                    .Include(s => s.Courses.Select(c => c.School))
+                    .FirstOrDefault(s => s.FirstName == "Brendan");
+
+                Assert.NotNull(brendan.Enrolments);
+                Assert.NotEmpty(brendan.Enrolments);
+            }
+        }
+
+        [Fact]
+        public void Run()
+        {
+            this.BDDfy();
+        }
+
+
+    }
+}
---
+++ SSW.DataOnion.UserGroupDemo/SSW.DataOnion.UserGroupDemo.VanillaEF.IntegrationTests/LazyLoadingOff.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SSW.DataOnion.UserGroupDemo.Domain;
+using TestStack.BDDfy;
+using Xunit;
+
+namespace SSW.DataOnion.UserGroupDemo.VanillaEF.IntegrationTests
+{
+    public class LazyLoadingOff
+    {
+
+
+        void GivenBrendanHasEnrolments()
+        {
+            DropDatabase.DropDb();
+            using (var context = new DemoContextFactory().Create())
+            {
+                var brendan = context.Students.FirstOrDefault(s => s.FirstName == "Brendan");
+
+                Assert.NotNull(brendan);
+
+                brendan.Enrolments.Add(new Enrolment()
+                {
+                    StartDate = DateTime.Now,
+                    EndDate = DateTime.Now.AddYears(1)
+                });
+
+                brendan.Enrolments.Add(new Enrolment()
+                {
+                    StartDate = DateTime.Now,
+                    EndDate = DateTime.Now.AddYears(1)
+                });
+
+                context.SaveChanges();
+            }
+        }
+
+
+        void ThenEnrolmentsIsNullWithLazyLoadingOff()
+        {
+            using (var context = new DemoContextFactory().Create())
+            {
+                context.Configuration.LazyLoadingEnabled = false;
+
+                var brendan = context.Students.FirstOrDefault(s => s.FirstName == "Brendan");
+                Assert.Null(brendan.Enrolments);
+            }
+        }
+
+        [Fact]
+        public void Run()
+        {
+            this.BDDfy();
+        }
+
+
+    }
+}
