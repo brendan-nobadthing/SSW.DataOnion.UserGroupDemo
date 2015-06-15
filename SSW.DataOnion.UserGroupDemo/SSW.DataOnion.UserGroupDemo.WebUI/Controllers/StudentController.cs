598ced9287058dbd705892a1abeef1a81baa5599
Brendan Richards <brendan_richards_msdn@hotmail.com>
2015-06-15 11:43:39 +1000

    web: added simple crud example

--- SSW.DataOnion.UserGroupDemo/SSW.DataOnion.UserGroupDemo.WebUI/Controllers/StudentController.cs
+++ SSW.DataOnion.UserGroupDemo/SSW.DataOnion.UserGroupDemo.WebUI/Controllers/StudentController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SSW.Data.Interfaces;
 using SSW.DataOnion.UserGroupDemo.DataInterfaces.Repositories;
+using SSW.DataOnion.UserGroupDemo.Domain;
 
 namespace SSW.DataOnion.UserGroupDemo.WebUI.Controllers
 {
@@ -14,10 +16,12 @@ namespace SSW.DataOnion.UserGroupDemo.WebUI.Controllers
     {
 
         private readonly IStudentRepository _studentRepository;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public StudentController(IStudentRepository studentRepository)
+        public StudentController(IStudentRepository studentRepository, IUnitOfWork unitOfWork)
         {
             _studentRepository = studentRepository;
+            _unitOfWork = unitOfWork;
         }
 
         // GET: Default
@@ -25,5 +29,33 @@ namespace SSW.DataOnion.UserGroupDemo.WebUI.Controllers
         {
             return View(_studentRepository.Get());
         }
+
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var student = _studentRepository.Find(id);
+            return View(student);
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(Student model)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                var entity = _studentRepository.Find(model.Id);
+                entity.FirstName = model.FirstName;
+                entity.LastName = model.LastName;
+
+                _unitOfWork.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
     }
 }
