a2dbed302a797e28d83e182470a18fed218d83a7
Brendan Richards <brendan_richards_msdn@hotmail.com>
2015-08-18 16:02:39 +1000

    Added octopack so I could test deployment with octopus deploy

---
+++ SSW.DataOnion.UserGroupDemo/SSW.DataOnion.UserGroupDemo.WebUI/BuildAndDeploy.bat
@@ -0,0 +1,5 @@
+﻿
+rem run this from the developer command prompt
+
+rem todo: update to https:// url
+msbuild ..\SSW.DataOnion.UserGroupDemo.sln /t:Build /p:RunOctoPack=true /p:OctoPackPublishPackageToHttp=http://octopus.ssw.com.au:8081/nuget/packages /p:OctoPackPublishApiKey=API-VPCIKFIVNNC8JG6ZZCSNLVPNL8
